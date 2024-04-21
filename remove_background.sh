dd195e789d20ca5682dd479232e34cd7c5bd1b8b
Sophie <70588905+probablySophie@users.noreply.github.com>
2024-04-21 19:34:53 +0800

    Create remove_background.sh

---
+++ remove_background.sh
@@ -0,0 +1,17 @@
+clear; printf "Running!\n"
+
+printf "> Cloning repo\n"
+git clone --quiet -b pull-model-weights-during-build https://github.com/nwautomator/rembg-webapp-tutorial
+
+printf "> Entering repo directory\n"
+cd rembg-webapp-tutorial
+
+printf "> Building docker image.  This will take a while...\n"
+docker build -t removebg -f Dockerfile . --quiet
+
+printf "> Leaving repo directory\n"
+cd ..
+
+printf "> Running"
+
+printf "\nRun $(tput setaf 13) docker run --rm -it --name remove-bg removebg $(tput sgr0) to run the image"

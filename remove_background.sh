8663cf4093b2859a09b3406973b21035d634ff7a
Sophie <70588905+probablySophie@users.noreply.github.com>
2024-04-24 18:46:44 +0800

    Added a cute little spinner

--- remove_background.sh
+++ remove_background.sh
@@ -7,7 +7,26 @@ printf "> Entering repo directory\n"
 cd rembg-webapp-tutorial
 
 printf "> Building docker image.  This will take a while...\n"
-docker build -t removebg -f Dockerfile . --quiet
+
+docker build -t removebg -f Dockerfile . --quiet & PID=$!
+
+# Cute little spinner time!
+SPINNER=('⠋' '⠇' '⡆' '⣄' '⣠' '⢰' '⠸' '⠙')
+let SPINNER_LOC=0
+
+while kill -0 $PID 2> /dev/null; do 
+	
+	echo -ne "\b\b${SPINNER[SPINNER_LOC]} ";
+
+	let SPINNER_LOC+=1;
+
+	if [ "${#SPINNER[@]}" -eq "${SPINNER_LOC}" ]; then
+		SPINNER_LOC=0
+	fi
+
+	sleep 0.1
+done
+
 
 printf "> Leaving repo directory\n"
 cd ..

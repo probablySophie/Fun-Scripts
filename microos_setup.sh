1dd8196d91b36e15c296db7f972a762f6cf6d2f3
Sophie <70588905+probablySophie@users.noreply.github.com>
2024-06-21 08:34:56 +0800

    you're not allowed to have --rm AND --restart=always :(

--- microos_setup.sh
+++ microos_setup.sh
@@ -7,7 +7,7 @@ transactional-update pkg in tmux
 podman volume create portainer_data # make a volume
 sudo systemctl enable --now podman.socket # not 100% sure what this is
 podman run \
-	--rm -d -p 9443:9443 \
+	-d -p 9443:9443 \
 	--privileged --restart=always \
 	-v /run/podman/podman.sock:/var/run/docker.sock:Z \
 	-v portainer_data:/data \
@@ -22,7 +22,7 @@ echo -e "[Unit] \n\
 Description=Spark service \n\
 \n\ 
 [Service] \n\
-ExecStart=/usr/bin/podman run --rm -d -p 9443:9443 --privileged --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v portainer_data:/data docker.io/portainer/portainer-ce:latest \n\
+ExecStart=/usr/bin/podman run -d -p 9443:9443 --privileged --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v portainer_data:/data docker.io/portainer/portainer-ce:latest \n\
 \n\
 [Install]\n\
 WantedBy=multi-user.target\n" >> /etc/systemd/system/podman-boot.service

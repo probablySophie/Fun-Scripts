cea8cb239e3abbd3f24380bff1214bb5d9871a28
Sophie <70588905+probablySophie@users.noreply.github.com>
2024-06-24 08:53:57 +0800

    Cleaning the restart stuff up
    SOMEONE didn't know about the podman-restart service :(

--- microos_setup.sh
+++ microos_setup.sh
@@ -17,15 +17,5 @@ podman run \
 # THIS IS CURRENTLY UNTESTED
 ln -s /run/podman/podman.sock /var/run/docker.sock
 
-# Create us a systemd service to always run portainer on boot
-echo -e "[Unit] \n\
-Description=Spark service \n\
-\n\ 
-[Service] \n\
-ExecStart=/usr/bin/podman run -d -p 9443:9443 --privileged --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v portainer_data:/data docker.io/portainer/portainer-ce:latest \n\
-\n\
-[Install]\n\
-WantedBy=multi-user.target\n" >> /etc/systemd/system/podman-boot.service
-
-# Turn on that systemd service
-systemctl enable podman-boot
+# Ask systemd to start podman on boot, and for podman to auto start our --restart=always containers
+systemctl enable podman-restart.service
